85e1bd7e7cf1a62bfb13e0b63683d4e0bd6c26a7
Denis Molodtsov <denis-ne@yandex.ru>
2022-03-31 20:16:21 -0400

    finished game

--- MazeGame.py
+++ MazeGame.py
@@ -4,6 +4,8 @@ import pygame
 import datetime
 from datetime import timedelta
 import sys
+from pathlib import Path
+import csv
 
 screen_width, screen_height = 800, 800
 pygame.init()
@@ -14,46 +16,42 @@ pygame.display.set_icon(programIcon)
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 
-# START MENU
+# ==================================== #
+#          START MENU
+# ==================================== #
 base_font = pygame.font.Font(None, 32)
-user_text = ''
+PlayerID = ''
 
-input_rect = pygame.Rect(200, 200, 140, 32)
+input_rect = pygame.Rect(300, 300, 140, 32)
 color_active = pygame.Color('lightskyblue3')
 color_passive = pygame.Color('gray15')
 color = color_passive
 
-active = False
 playerNameEntered = False
 while playerNameEntered == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if input_rect.collidepoint(event.pos):
-                active = True
-            else:
-                active = False
 
         if event.type == pygame.KEYDOWN:
-            if active == True:
-                if event.key == pygame.K_RETURN:
-                    playerNameEntered = True
-                elif event.key == pygame.K_BACKSPACE:
-                    user_text = user_text[:-1]                
-                else:
-                    user_text += event.unicode
+            if event.key == pygame.K_RETURN:
+                playerNameEntered = True
+            elif event.key == pygame.K_BACKSPACE:
+                PlayerID = PlayerID[:-1]
+            else:
+                PlayerID += event.unicode
     screen.fill((0, 0, 0))
 
-    pygame.draw.rect(screen, color, input_rect, 2)
+    msg = base_font.render("Player:", 1, (255, 255, 0))
+    screen.blit(msg, (219, 305))
 
-    text_surface = base_font.render(user_text, True, (255, 255, 255))
+    pygame.draw.rect(screen, color, input_rect, 2)
+    text_surface = base_font.render(PlayerID, True, (255, 255, 255))
     screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
     pygame.display.flip()
-    input_rect.w = max(100, text_surface.get_width()+10)
+    input_rect.w = max(120, text_surface.get_width()+10)
     clock.tick(60)
-
 
 # ==================================== #
 #          MAZE GAME BEGINS
@@ -68,7 +66,7 @@ attempts = 1
 
 run = True
 font = pygame.font.Font(None, 60)
-pygame.mouse.set_pos((25, 500))
+pygame.mouse.set_pos((25, 650))
 
 font = pygame.font.Font(None, 60)
 gameStartTime = datetime.datetime.now()
@@ -89,7 +87,7 @@ while run:
 
     pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(
         0, 0, screen_width, screen_height))
-    screen.blit(bg, (0, 100))
+    screen.blit(bg, (0, 150))
 
     mousePosition = pygame.mouse.get_pos()
     color = screen.get_at(mousePosition)
@@ -97,12 +95,12 @@ while run:
 
     # if color beneath the mouse cursor is re
     if color[0] > 250 and color[1] < 20 and color[2] < 20:
-        msg = font.render("YOU WON", 1, (255, 255, 0))
-        screen.blit(msg, (300, 50))
+        # You WON
+        run = False
 
     # if color beneath the mouse cursor is black
     if color == (0, 0, 0, 255) and mousePosition != (0, 0):
-        pygame.mouse.set_pos((25, 550))
+        pygame.mouse.set_pos((25, 650))
 
         current_time = datetime.datetime.now()
         if 'timeoutStartTime' in locals():
@@ -124,5 +122,49 @@ while run:
     attemptsMessage = font.render(
         "Attempts: " + str(attempts), 1, (255, 255, 0))
     screen.blit(attemptsMessage, (25, 750))
+
+    pygame.display.update()
+
+# ==================================== #
+#          GAME ENDS
+# ==================================== #
+
+run = True
+font = pygame.font.Font(None, 100)
+
+while run:
+    clock.tick(15)
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+             if event.key == pygame.K_RETURN:
+                 # Time to save to CSV
+                now = datetime.datetime.now()
+                dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+                fields = [PlayerID, attempts, timeoutDifference.seconds, dt_string]
+                with open(r'results.csv', 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(fields)
+                pygame.quit()
+                sys.exit()
+
+        if event.type == pygame.QUIT:
+            # Time to save to CSV
+            now = datetime.datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+            fields = [PlayerID, attempts, timeoutDifference.seconds, dt_string]
+            with open(r'results.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(fields)
+            pygame.quit()
+            sys.exit()
+
+    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(
+        0, 0, screen_width, screen_height))
+
+    msg = font.render("YOU WON!", 1, (255, 255, 0))
+    screen.blit(msg, (200, 350))
 
     pygame.display.update()
